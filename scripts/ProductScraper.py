4e2c73bb2ada072bb3ed5de64ae388a184d9c373
mkuczynski11 <mkuczynski11.kontakt@wp.pl>
2021-11-07 13:00:56 +0100

    stock + delivery time + product features

--- scripts/ProductScraper.py
+++ scripts/ProductScraper.py
@@ -10,142 +10,13 @@ class ProductScraper(scrapy.Spider):
         'https://www.lampynowodvorski.pl/',
     ]
     FILE_NAME = 'products.csv'
-    ID = 'ID'
-    ACITVE = 'Aktywny (0 lub 1)'
-    NAME = 'Nazwa'
-    CATEGORIES = 'Kategorie (x,y,z...)'
-    PRICE_WITHOUT_TAX = 'Cena bez podatku. (netto)'
-    PRICE_WITH_TAX = 'Cena zawiera podatek. (brutto)'
-    ID_TAX = 'ID reguły podatku'
-    COST = 'Koszt własny'
-    IN_SALE = 'W sprzedaży (0 lub 1)'
-    DISCOUNT = 'Wartość rabatu'
-    DISCOUNT_PERCENT = 'Procent rabatu'
-    DISCOUNT_START = 'Rabat od dnia (rrrr-mm-dd)'
-    DISCOUNT_END = 'Rabat do dnia (rrrr-mm-dd)'
-    ID_HASH = 'Indeks #'
-    PROVIDER_CODE = 'Kod dostawcy'
-    PROVIDER = 'Dostawca'
-    BRAND = 'Marka'
-    EAN13 = 'kod EAN13'
-    UPC = 'kod kreskowy UPC'
-    MPN = 'MPN'
-    ECO_TAX = 'Podatek ekologiczny'
-    WIDTH = 'Szerokość'
+    NOT_INCLUDED = ['Outlet']
+
+    EAN = 'EAN'
     HEIGHT = 'Wysokość'
-    DEPTH = 'Głębokość'
-    WEIGHT = 'Waga'
-    DELIVERY_TIME_AVAILABLE = 'Czas dostawy produktów dostępnych w magazynie:'
-    DELIVERY_TIME_SOLD_WITH_RESERVATION = 'Czas dostawy wyprzedanych produktów z możliwością rezerwacji:'
-    AMOUNT = 'Ilość'
-    MIN_AMOUNT = 'Minimalna ilość'
-    LOW_AMOUNT_IN_STOCK = 'Niski poziom produktów w magazynie'
-    EMAIL_WHEN_LOWER_THAN = 'Wyślij do mnie e-mail, gdy ilość jest poniżej tego poziomu'
-    VISIBILITY = 'Widoczność'
-    ADDITIONAL_DELIVERY_COST = 'Dodatkowe koszty przesyłki'
-    PRICE_FOR_UNIT = 'Cena za jednostkę'
-    SUMMARY = 'Podsumowanie'
-    DESCRIPTION = 'Opis'
-    TAGS = 'Tagi (x,y,z...)'
-    META_TITLE = 'Meta-tytuł'
-    META_KEYWORDS = 'Słowa kluczowe meta'
-    META_DESCRIPTION = 'Opis meta'
-    URL = 'Przepisany URL'
-    LABEL_WHEN_IN_STOCK = 'Etykieta, gdy w magazynie'
-    LABEL_IF_ALLOWED_ANOTHER_ORDER = 'Etykieta kiedy dozwolone ponowne zamówienie'
-    AVAILABLE_FOR_ORDER = 'Dostępne do zamówienia (0 = Nie, 1 = Tak)'
-    AVAILABLE_DATE = 'Data dostępności produktu'
-    CREATION_DATE = 'Data wytworzenia produktu'
-    SHOW_PRICE = 'Pokaż cenę (0 = Nie, 1 = Tak)'
-    PHOTO_URLS = 'Adresy URL zdjęcia (x,y,z...)'
-    PHOTO_ALTERNATIVE_TEXT = 'Tekst alternatywny dla zdjęć (x,y,z...)'
-    DELETE_EXISTING_PHOTOS = 'Usuń istniejące zdjęcia (0 = Nie, 1 = Tak)'
-    TRAIT = 'Cecha(Nazwa:Wartość:Pozycja:Indywidualne)'
-    AVAILABLE_ONLY_ONLINE = 'Dostępne tylko online (0 = Nie, 1 = Tak)'
-    STATE = 'Stan:'
-    CONIGURABLE = 'Konfigurowalny (0 = Nie, 1 = Tak)'
-    CAN_INSERT_FILES = 'Można wgrywać pliki (0 = Nie, 1 = Tak)'
-    TEXT_FIELDS = 'Pola tekstowe (0 = Nie, 1 = Tak)'
-    ACTION_WHEN_OUT_OF_STOCK = 'Akcja kiedy brak na stanie'
-    VIRTUAL_PRODUCT = 'Wirtualny produkt (0 = No, 1 = Yes)'
-    FILE_URL = 'Adres URL pliku'
-    ALLOWED_DOWNLOADS = 'Ilość dozwolonych pobrań'
-    EXPIRE_DATE = 'Data wygaśnięcia (rrrr-mm-dd)'
-    DAYS_COUNT = 'Liczba dni'
-    ID_OR_SHOP_NAME = 'ID / Nazwa sklepu'
-    EXTENDED_STOCK_MANAGEMENT = 'Zaawansowane zarządzanie magazynem'
-    DEPENDENT_ON_STOCK_STATE = 'Zależny od stanu magazynowego'
-    STOCK = 'Magazyn'
-    ACCESORIES = 'Akcesoria (x,y,z...)'
+    WIDTH = 'Szerokość'
 
-    FIRST_ROW = [ID,
-    ACITVE,
-    NAME,
-    CATEGORIES,
-    PRICE_WITHOUT_TAX,
-    PRICE_WITH_TAX,
-    ID_TAX,
-    COST,
-    IN_SALE,
-    DISCOUNT,
-    DISCOUNT_PERCENT,
-    DISCOUNT_START,
-    DISCOUNT_END,
-    ID_HASH,
-    PROVIDER_CODE,
-    PROVIDER,
-    BRAND,
-    EAN13,
-    UPC,
-    MPN,
-    ECO_TAX,
-    WIDTH,
-    HEIGHT,
-    DEPTH,
-    WEIGHT,
-    DELIVERY_TIME_AVAILABLE,
-    DELIVERY_TIME_SOLD_WITH_RESERVATION,
-    AMOUNT,
-    MIN_AMOUNT,
-    LOW_AMOUNT_IN_STOCK,
-    EMAIL_WHEN_LOWER_THAN,
-    VISIBILITY,
-    ADDITIONAL_DELIVERY_COST,
-    PRICE_FOR_UNIT,
-    SUMMARY,
-    DESCRIPTION,
-    TAGS,
-    META_TITLE,
-    META_KEYWORDS,
-    META_DESCRIPTION,
-    URL,
-    LABEL_WHEN_IN_STOCK,
-    LABEL_IF_ALLOWED_ANOTHER_ORDER,
-    AVAILABLE_FOR_ORDER,
-    AVAILABLE_DATE,
-    CREATION_DATE,
-    SHOW_PRICE,
-    PHOTO_URLS,
-    PHOTO_ALTERNATIVE_TEXT,
-    DELETE_EXISTING_PHOTOS,
-    TRAIT,
-    AVAILABLE_ONLY_ONLINE,
-    STATE,
-    CONIGURABLE,
-    CAN_INSERT_FILES,
-    TEXT_FIELDS,
-    ACTION_WHEN_OUT_OF_STOCK,
-    VIRTUAL_PRODUCT,
-    FILE_URL,
-    ALLOWED_DOWNLOADS,
-    EXPIRE_DATE,
-    DAYS_COUNT,
-    ID_OR_SHOP_NAME,
-    EXTENDED_STOCK_MANAGEMENT,
-    DEPENDENT_ON_STOCK_STATE,
-    STOCK,
-    ACCESORIES]
-
+    current_id = 100
     def parse(self, response):
 
         # Removing file if it exists, so that new values can be appended
@@ -154,12 +25,16 @@ class ProductScraper(scrapy.Spider):
             os.remove(self.FILE_NAME)
         except:
             pass
-
-        first_category_page_links = response.css('ul.menu-cn > li a')
+        
+        # Following all main categories pages
+        first_category_page_links = response.css('nav#menu3 > ul.menu-cn > li > a')
         yield from response.follow_all(first_category_page_links, self.parse_products_from_category)
 
     def parse_products_from_category(self, response):
-
+        # We do not want to scrap certain categories
+        title = response.css('body > main > header > h1::text').get()
+        if title in self.NOT_INCLUDED:
+            return
         for item in response.css('div#page > div#products > div.list > div'):
             # Product internal ID
             href = item.css('div.photo > a::attr(href)').get()
@@ -167,7 +42,15 @@ class ProductScraper(scrapy.Spider):
             # Fully functional link to product page
             product_page = response.urljoin(href)
 
+            # Following product page
             yield scrapy.Request(product_page, callback=self.parse_product)
+        
+        # Next page following
+        next_page = response.css('div#pagesBefore > ul > li > a.pNext::attr(href)').get()
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(next_page_url, self.parse_products_from_category)
+
 
     def parse_product(self, response):
 
@@ -176,20 +59,99 @@ class ProductScraper(scrapy.Spider):
 
         product_name = info_box.css('h1::text').get()
 
+        # Product categories and url parsing
         breadcrumb = info_box.css('div#breadcrumb > p')
         categories = self.parse_product_categories(breadcrumb)
+        url = self.parse_product_link(breadcrumb)
+        
+        # Product features parsing
+        keys = info_box.css('dl > dt::text')
+        index = 1
+        ean = None
+        description = None
+        height = None
+        width = None
+        features = []
+        # For each dt we are trying to parse it to the write column
+        for key in keys:
+            value = info_box.css(f'dl > dd:nth-of-type({index})::text')
+            key_raw = key.get()[:-1]
+            value_raw = value.get()
+            if key_raw == self.EAN:
+                ean = int(value_raw)
+            elif key_raw == '':
+                description = value_raw
+            elif key_raw == self.HEIGHT:
+                height = value_raw
+            elif key_raw == self.WIDTH:
+                width = value_raw
+            else:
+                features.append(f'{key_raw}:{value_raw}')
+            index += 1
 
-        print(f'Product: {product_name}')
-        print(f'Categories: {categories}')
+        # Product stock amount and delivery time parsing
+        # TODO: on their site there is a note on additional amount in stock with other delivery time
+        # resolve it
+        in_stock = 0
+        delivery_time = None
+        stock_em = info_box.css('p.stock > em::text').get()
+        if stock_em:
+            in_stock = info_box.css('p.stock::text').get()
+            delivery_time = in_stock
+            in_stock = in_stock.replace('szt.', '')
+            in_stock = int(in_stock)
+            if info_box.css('p.stock > span::text').get():
+                delivery_time = info_box.css('p.stock > span::text').get()
+                delivery_time = delivery_time.split(' ')[3:]
+                tmp = ''
+                for item in delivery_time:
+                    tmp += item + ' '
+                delivery_time = tmp[:-1]
+        else:
+            in_stock = info_box.css('p.stock::text').get()
+            delivery_time = in_stock
+            if in_stock is not None:
+                in_stock = in_stock.replace('Na stanie: ', '')
+                in_stock = in_stock.replace('Dostępnych: ', '')
+                in_stock = in_stock.split(' ')[0]
+                in_stock = int(in_stock)
+                if info_box.css('p.stock > span::text').get():
+                    delivery_time = info_box.css('p.stock > span::text').get()
+                    delivery_time = delivery_time.split(' ')[3:]
+                    tmp = ''
+                    for item in delivery_time:
+                        tmp += item + ' '
+                    delivery_time = tmp[:-1]
+                else:
+                    delivery_time = delivery_time.split('/ ')[1].split(' ')[2:]
+                    tmp = ''
+                    for item in delivery_time:
+                        tmp += item + ' '
+                    delivery_time = tmp[:-1]
 
-        # with open('products.csv', mode='a') as products_file:
-        #     products_writer = csv.writer(products_file, delimiter=';')
-        #     products_writer.writerow(['Datalallaa','Moooore'])
-            # products_writer.writerow(self.FIRST_ROW)
+
+        with open(self.FILE_NAME, mode='a+') as products_file:
+            products_writer = csv.writer(products_file, delimiter=';')
+            id = self.current_id
+            self.current_id += 1
+            products_writer.writerow([id, product_name, categories, url, ean, description, width, height,  self.features_to_string(features), in_stock])
 
     def parse_product_categories(self, breadcrumb):
         categories = []
         for a in breadcrumb.css('p  > a::text'):
             categories.append(a.get())
         # Last breadcrumb is product name
-        return categories[:-1]
+        categories = categories[:-1]
+        categories_string = ''
+        for category in categories:
+            categories_string += category + '//'
+        return categories_string[:-2]
+
+    def parse_product_link(self, breadcrumb):
+        return breadcrumb.css('p > a:last-child::attr(href)').get()
+
+    def features_to_string(self, features):
+        features_string = ''
+        for feature in features:
+            features_string += feature + '//'
+        return features_string[:-2]
