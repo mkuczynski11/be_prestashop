507101381ce8cac593b054e946f801f0c357ce16
mkuczynski11 <mkuczynski11.kontakt@wp.pl>
2022-01-13 12:56:20 +0100

    specific values

--- database-into-container.sh
+++ database-into-container.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# <pod-name> to nazwa kontenera
-
 # copy dump
-docker cp ./db-dumps/*.sql <pod-name>:/tmp/be_180199.sql
+docker cp ./db-dumps/*.sql wjk9pz7gh2s9:/tmp/be_180199.sql
 
 # copy database loading script
-docker cp ./db-dump.sh <pod-name>:/tmp/db-dump_180199.sh
+docker cp ./db-dump.sh wjk9pz7gh2s9:/tmp/db-dump_180199.sh
 
 # grant pirvilages to the script in order to execute it
-docker exec <pod-name> chmod +x /tmp/db-dump_180199.sh
+docker exec wjk9pz7gh2s9 chmod +x /tmp/db-dump_180199.sh
 
 # execute the script
-docker exec <pod-name> /tmp/db-dump.sh
+docker exec wjk9pz7gh2s9 /tmp/db-dump.sh
 
 # remove files
-docker exec <pod-name> rm /tmp/db-dump_180199.sh
-docker exec <pod-name> rm /tmp/be_180199.sql
+docker exec wjk9pz7gh2s9 rm /tmp/db-dump_180199.sh
+docker exec wjk9pz7gh2s9 rm /tmp/be_180199.sql
